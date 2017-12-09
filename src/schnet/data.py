Fixed force gradient

src/schnet/data.py
@@ -5,7 +5,6 @@ from random import shuffle
 import numpy as np
 import tensorflow as tf
 from ase.db import connect
-
 from schnet.atoms import collect_neighbors, IsolatedAtomException
 
 
@@ -117,6 +116,8 @@ class ASEReader:
         if type(idx) is int:
             idx = [idx]
 
+        #start = time.time()
+
         with connect(self.asedb) as conn:
             data = {
                 'aid': [],
@@ -169,6 +170,9 @@ class ASEReader:
                     data[prop].append(row.data[prop].astype(np.float32))
 
         data = {p: np.concatenate(b, axis=0) for p, b in data.items()}
+
+        #print('Batch time:', time.time()-start)
+
         return data
 
     def get_property(self, pname, idx):

src/schnet/forces.py
@@ -5,9 +5,10 @@ from schnet.data import get_atoms_input
 
 def predict_energy_forces(model, data):
     atoms_input = get_atoms_input(data)
-    Ep = model(*atoms_input)
-    Fp = -tf.convert_to_tensor(
-        tf.gradients(tf.reduce_sum(Ep), atoms_input[1])[0])
+    g = tf.get_default_graph()
+    with g.gradient_override_map({"Tile": "TileDense"}):
+        Ep = model(*atoms_input)
+        Fp = -tf.gradients(tf.reduce_sum(Ep), atoms_input[1])[0]
     return Ep, Fp
 
 
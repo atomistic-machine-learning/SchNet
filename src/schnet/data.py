Fixed force gradient

src/schnet/data.py
@@ -1,4 +1,4 @@
-import logging
+import logg     validate_indices, name, axis)ing
 import threading
 from random import shuffle
 
@@ -7,7 +7,7 @@ import tensorflow as tf
 from ase.db import connect
 
 from schnet.atoms import collect_neighbors, IsolatedAtomException
-
+import time
 
 def generate_neighbor_dataset(asedb, nbhdb, cutoff):
     '''
@@ -117,6 +117,8 @@ class ASEReader:
         if type(idx) is int:
             idx = [idx]
 
+        #start = time.time()
+
         with connect(self.asedb) as conn:
             data = {
                 'aid': [],
@@ -169,6 +171,9 @@ class ASEReader:
                     data[prop].append(row.data[prop].astype(np.float32))
 
         data = {p: np.concatenate(b, axis=0) for p, b in data.items()}
+
+        #print('Batch time:', time.time()-start)
+
         return data
 
     def get_property(self, pname, idx):

src/schnet/nn/train.py
@@ -48,11 +48,13 @@ class EarlyStopping:
             np.savez(self.loss_path, loss=self.best_loss, step=self.best_step)
 
         self.saver = tf.train.Saver()
-        self.train_writer = tf.summary.FileWriter(self.train_dir)
         self.val_writer = tf.summary.FileWriter(self.val_dir)
         self.start_iter = 0
 
     def train(self, sess, coord, max_steps):
+        self.train_writer = tf.summary.FileWriter(self.train_dir,
+                                                  graph=sess.graph)
+
         # restore
         chkpt = tf.train.latest_checkpoint(self.chkpt_dir)
         if chkpt is not None:
@@ -91,7 +93,7 @@ class EarlyStopping:
                     )
                     loss.append(result[0])
                     errors.append(result[1:])
-                
+
                 vloss, summary = self.summary_fn(loss, list(zip(*errors)))
                 self.val_writer.add_summary(summary, global_step=step)
                 if vloss < self.best_loss:
@@ -106,7 +108,7 @@ class EarlyStopping:
 
 
 def build_train_op(loss, optimizer, global_step, moving_avg_decay=0.99,
-          dependencies=[]):
+                   dependencies=[]):
     grads = optimizer.compute_gradients(loss)
     apply_gradient_op = optimizer.apply_gradients(grads,
                                                   global_step=global_step)
@@ -117,7 +119,7 @@ def build_train_op(loss, optimizer, global_step, moving_avg_decay=0.99,
 
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(
-                            [apply_gradient_op,
-                             variables_averages_op] + update_ops + dependencies):
+            [apply_gradient_op,
+             variables_averages_op] + update_ops + dependencies):
         train_op = tf.no_op(name='train')
     return train_op